d19845cf71e4a79ec31525b8c143909974dfee1a
andreassoegaard <andreas@codeful.dk>
2023-03-02 15:47:26 +0100

    updates

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ app.use((req, res, next) => {
   // Request headers you wish to allow
   res.setHeader(
     "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
 
   // Set to true if you need the website to include cookies in the requests sent
